4f69158c5d1a03b535f456655114f82f1e0561f7
EJGR1906 <110251679+EJGR1906@users.noreply.github.com>
2022-08-03 17:15:19 -0400

    Add files via upload

---
+++ Clase 10/Ejercicio1Clase10.py
@@ -0,0 +1,40 @@
+
+import tkinter
+def main():
+    #Creo la ventana
+    window=tkinter.Tk()
+    window.title('Ejercicio1')
+
+    #Definimos las funciones
+    def MostrarValor():
+        print(valorRadiobutton.get())
+    def Reiniciar():
+        valorRadiobutton.set(0)
+        MostrarValor()
+    #Creamos el frame
+    frame=tkinter.Frame(window,padx=20,pady=20)
+    frame.pack()
+
+    #Creamos los radiobuttons
+    valorRadiobutton=tkinter.IntVar()
+    radiobutton1=tkinter.Radiobutton(frame,text='Radiobutton 1',variable=valorRadiobutton,value=1,command=MostrarValor)
+    radiobutton1.pack()
+
+    radiobutton2=tkinter.Radiobutton(frame,text='Radiobutton 2',variable=valorRadiobutton,value=2,command=MostrarValor)
+    radiobutton2.pack()
+
+    radiobutton3=tkinter.Radiobutton(frame,text='Radiobutton 3',variable=valorRadiobutton,value=3,command=MostrarValor)
+    radiobutton3.pack()
+
+    radiobutton4=tkinter.Radiobutton(frame,text='Radiobutton 4',variable=valorRadiobutton,value=4,command=MostrarValor)
+    radiobutton4.pack()
+
+    #Creamos el button de reinicio
+    button=tkinter.Button(frame,text='Reinicio',command=Reiniciar)
+    button.pack()
+
+
+    window.mainloop()
+
+if __name__=='__main__':
+    main()
